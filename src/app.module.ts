2442b6d9799582bc2c9360314a367eb78838c1c1
yhansol145 <yhansol145@gmail.com>
2026-03-17 22:41:20 +0900

    fix: ServeStaticModule process.cwd() 경로 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { StockModule } from './stock/stock.module';
 import { MarketModule } from './market/market.module';
 import { NewsModule } from './news/news.module';
 import { RecommendationModule } from './recommendation/recommendation.module';
 import { CronModule } from './cron/cron.module';
 
-const isVercel = process.env.VERCEL === '1';
-
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ...(isVercel
-      ? []
-      : [ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') })]),
+    ServeStaticModule.forRoot({ rootPath: `${process.cwd()}/public` }),
     StockModule,
     MarketModule,
     NewsModule,
